95ed4063b23bee8e37109c76e20d196f68ed6ac1
Ian MacDonald @ epicodus <ian@iancmacdonald.com>
2015-07-27 12:56:12 -0700

    Complete challenge 3

--- program.js
+++ program.js
@@ -1,13 +1,30 @@
-// var fs = require('fs');
+var fs = require('fs');
 
-// file = fs.readFileSync(argv[2]);
 
-function addArgv(argvs) {
-  var sum = 0;
-  for (var i = 2; i < argvs.length; i++) {
-    sum += Number(argvs[i]);
+// Challenge 3
+file = fs.readFile(process.argv[2], 'utf8', function readArgvFile(err, data) {
+  if (err) {
+    console.error(err)
+  } else {
+    splitFile = data.split('\n');
+    console.log(splitFile.length - 1);
   }
-  return sum;
-}
+});
 
-console.log(addArgv(process.argv));
+// Challenge 2
+// file = fs.readFileSync(process.argv[2]).toString();
+//
+// splitFile = file.split('\n');
+//
+// console.log(splitFile.length - 1);
+
+// Challenge 1
+// function addArgv(argvs) {
+//   var sum = 0;
+//   for (var i = 2; i < argvs.length; i++) {
+//     sum += Number(argvs[i]);
+//   }
+//   return sum;
+// }
+//
+// console.log(addArgv(process.argv));
